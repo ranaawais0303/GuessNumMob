48e5b3dbbb73e2939db8901c2cabc430ddcbec1c
Rana_awais <z.manzoor@pioneerlogics.com>
2022-11-22 01:42:44 -0800

    improving the layout of buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,9 +12,13 @@ function StartGameScreen(props) {
         autoCapitalize="none"
         autoCorrect={false}
       />
-      <View style={styles.buttonContainer}>
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
       </View>
     </View>
   );
@@ -24,6 +28,8 @@ export default StartGameScreen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    // justifyContent: "center",
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -43,8 +49,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
   },
-  // buttonContainer: {
-  //   flexDirection: "row",
-  //   justifyContent: "center",
-  // },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
 });

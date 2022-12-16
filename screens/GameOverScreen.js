d7c2bb5e5519946ce20bab8106d717a3acbdffdf
Rana_awais <z.manzoor@pioneerlogics.com>
2022-12-15 22:40:58 -0800

    set image width and height and radius dinamically using dimentions width

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
 import Colors from "../constants/colors";
@@ -34,6 +34,8 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
 }
 
 export default GameOverScreen;
+
+const deviceWidth = Dimensions.get("window").width;
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
@@ -42,9 +44,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: deviceWidth < 380 ? 150 : 300,
+    height: deviceWidth < 380 ? 150 : 300,
+    borderRadius: deviceWidth < 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: "hidden",
